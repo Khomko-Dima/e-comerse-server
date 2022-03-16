416ae27a0ca620badc33f41fe37d38c7a96a555d
Dima Khomko <khomko-dima@mail.ru>
2022-03-16 22:58:50 +0300

    init

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express,{Express} from 'express'
+import {Server} from 'http'
+export class App {
+    app: Express;
+    server: Server
+    port: number
+
+    constructor() {
+        this.app = express();
+        this.port = 8000;
+    }
+
+    useRoutes() {
+        this.app.use('/users', (req, res) => {
+            console.log(req);
+            
+        })
+    }
+
+    public async init() {
+        this.useRoutes();
+        this.server = this.app.listen(this.port)
+        console.log(`Server start on port: ${this.port}`);
+        
+    }
+}
