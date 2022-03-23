91cf08203a08ccc43c91f4f8b6d6c64f81e6156c
Khomko Dima <khomko-dima@mail.ru>
2022-03-23 10:55:46 +0300

    add: jwt token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,15 @@ import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from "./dto/user-registr.dto";
 import { IUserService } from "./users.service.interface";
 import { ValidateMiddleware } from "../common/validate.middleware";
+import {sign} from 'jsonwebtoken'
+import { IConfigService } from "../config/config.service.interface";
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private usersService: IUserService
+		@inject(TYPES.UserService) private usersService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -38,7 +41,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(401, 'Ошибка авторизации', 'login'));
 		}
-		this.ok(res, {})
+		const jwt = await this.signJWT(body.email, this.configService.get('SECRET_JWT'))
+		this.ok(res, {jwt})
 	}
 	async register({ body }: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): Promise<void> {
 		const result = await this.usersService.createUser(body)
@@ -47,6 +51,20 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'Такой пользователь уже существует'))
 		}
 		this.ok(res, {email: result.email, name: result.name, id: result.id});
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{email, iat: Math.floor(Date.now() / 1000)},
+				secret,
+				{algorithm: 'HS256'},
+				(err, token) => {
+					if (err)reject(err)
+					resolve(token as string)
+				}
+			);
+		})
 	}
 }
 
